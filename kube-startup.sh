6865cfa840659e5322034b24207c14e2144b9601
Edmond Zalenski <ezalen2@uic.edu>
2017-12-12 18:25:08 -0600

    get cluster in the beginning

--- kube-startup.sh
+++ kube-startup.sh
@@ -4,9 +4,35 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# wait for nodes to come up
+sleep 30
+
+# get cluster
+sudo ip -s -s neigh flush all
+sudo ping -b 10.0.0.255 -c 5 #replace with own ipaddress .255
+IPS=$(arp -e | grep eth0 | awk '{print $1}')
+declare -A IP_SET
+declare -A PI_IPS
+
+
+for IP in $IPS; do
+    status=$(ssh -o BatchMode=yes -o ConnectTimeout=5 $IP echo ok 2>&1)
+    if [[ $status == ok && ! ${IP_SET["$IP"]+abc} ]] ; then
+        HOSTNAME=$(ssh $IP hostname)
+        IP_SET+=(["$IP"]=true)
+        PI_IPS+=("$IP")
+    fi
+done;
+
+for IP in $PI_IPS; do
+    echo $IP
+done;
+
+exit 0
 # reset the cluster
 rm -rf $HOME/.kube
 sudo kubeadm reset
+
 
 # setup cluster master
 JOIN_COMMAND=$(sudo kubeadm init --pod-network-cidr 10.244.0.0/16 --token-ttl 0 | grep " kubeadm join")
@@ -20,10 +46,6 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 curl -sSL https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml | sed "s/amd64/arm64/g" | kubectl create -f -
 
 # get slave local ips
-sudo ip -s -s neigh flush all
-sudo ping -b 10.0.0.255 -c 5 #replace with own ipaddress .255
-PI_IPS=$(arp -e | grep eth0 | awk '{print $1}')
-
 for IP in $PI_IPS; do
     status=$(ssh -o BatchMode=yes -o ConnectTimeout=5 $IP echo ok 2>&1)
     if [[ $status == ok ]] ; then
